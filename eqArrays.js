27cd13b726896a2773c396d3a6e31e1be46cc0df
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-05-21 15:25:01 -0500

    compare two arrays and check if they match

---
+++ eqArrays.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected){
+  if(actual === expected){
+  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else{  
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = function(a, b) {
+  for (let i = 0; i < a.length; i++) {
+    firstArray = a[i];
+    secondArray = b[i];
+  }
+  if (firstArray === secondArray) {
+    return true;
+  } else {
+    return false;
+  }
+  
+};
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
