de9a36f9ff4e48d419978c7d27ed4a8183a2c164
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-05-21 17:03:29 -0500

    adding a file to check if arrays match

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+const eqArrays = function(a, b) {
+  let firstArray = "";
+  let secondArray = "";
+  if(a.length === b.length){
+    for (let i = 0; i < a.length; i++) {
+    firstArray = a[i];
+    secondArray = b[i];
+    if (firstArray !== secondArray) {
+    return false;
+    }
+    }
+  return true;
+  } else {
+  return false;
+  }
+};
+
+function assertArraysEqual(actual, expected) {
+  if(eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    } else {  
+      console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+    }
+}
+
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+assertArraysEqual([1, 2, 3], [1, 2, 3]); 
