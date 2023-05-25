8424ed4dfd71cd241746c3525be6d2ee5b2295f7
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-05-24 19:38:58 -0500

    finding values in objects

---
+++ findKeyByValue.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected){
+  if(actual === expected){
+  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else{  
+    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+const findKeyByValue = function (bestTVShowsByGenre, value) {
+  for (let i in bestTVShowsByGenre) {
+    if (bestTVShowsByGenre[i] == value) {
+      return i;
+    }
+  }
+}
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
