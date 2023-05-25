b89765a42649bf61134601955b7eb340838288da
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-05-24 19:23:25 -0500

    Added assertArraysEqual

--- letterPositions.js
+++ letterPositions.js
@@ -1,10 +1,28 @@
-const assertEqual = function(actual, expected){
-  if(actual === expected){
-  console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
-  } else{  
-    console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+const eqArrays = function(a, b) {
+  let firstArray = "";
+  let secondArray = "";
+  if(a.length === b.length){
+    for (let i = 0; i < a.length; i++) {
+    firstArray = a[i];
+    secondArray = b[i];
+    if (firstArray !== secondArray) {
+    return false;
+    }
+    }
+  return true;
+  } else {
+  return false;
   }
 };
+
+function assertArraysEqual(actual, expected) {
+  if(eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    } else {  
+      console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+    }
+};
+
 
 const letterPositions = function(sentence) {
   const results = {};
@@ -22,21 +40,28 @@ const letterPositions = function(sentence) {
     }
   })
   
-
   console.log(results);
-  /*let array = [];
-  let i = 0;
-  for(i = 0; i < sentence.length; i++) {
-    array.push(i);
-    if (sentence[i] !== " ") {    
-      results[sentence[i]] = array.slice(i);
-    }*/
-  // }
- 
-  // logic to update results here
-  // console.log(results);
+  return results;
 };
 
 
 letterPositions("lighthouse in the house");
 letterPositions("hello");
+assertArraysEqual(letterPositions("hello").e, [1]);
+
+
+/*let array = [];
+  let i = 0;
+  for(i = 0; i < sentence.length; i++) {
+    array.push(i);
+    if (sentence[i] !== " ") {   
+      if(!results[sentence[i]]) {
+        results[letter] = [];
+      } 
+      results[sentence[i]].push(i);
+    }*/
+  // }
+ 
+  // logic to update results here
+  // console.log(results);
+  
