c22ede1bd0fa7304858665a693b5a52523467102
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-05-26 22:49:52 -0500

    get output until specific element of array

---
+++ takeUntil.js
@@ -0,0 +1,54 @@
+const eqArrays = function(a, b) {
+  let firstArray = "";
+  let secondArray = "";
+  if(a.length === b.length){
+    for (let i = 0; i < a.length; i++) {
+    firstArray = a[i];
+    secondArray = b[i];
+    if (firstArray !== secondArray) {
+    return false;
+    }
+    }
+  return true;
+  } else {
+  return false;
+  }
+};
+
+function assertArraysEqual(actual, expected) {
+  if(eqArrays(actual, expected)) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    } else {  
+      console.log(`🛑🛑🛑Assertion failed: ${actual} !== ${expected}`);
+    }
+};
+
+const takeUntil = function(array, callback) {
+  let c = 0;
+  let results = [];
+  array.forEach(function (a, b) {
+    if(callback(a)) {
+      c = b;
+    }
+  
+  });
+  array.map(function(a, b) {
+    if( b < c) {
+      results.push(array[b]);
+    }
+  })
+  return results;
+  }
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+assertArraysEqual(results2, [ "I've", 'been', 'to', 'Hollywood' ]);
