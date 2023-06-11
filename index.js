c965c5aa82561c4df512f7c966b91d6aef628467
Jasdeep Kaur <jasdeepkaur1010@gmail.com>
2023-06-11 00:21:24 -0500

    final commit

--- index.js
+++ index.js
@@ -4,12 +4,34 @@ const middle = require('./middle');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
 const eqArrays = require('./eqArrays');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const without = require('./without');
+const takeUntil = require('./takeUntil');
 
 module.exports = {
   assertArraysEqual,
   assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
   eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
   head,
+  letterPositions,
+  map,
   tail,
-  middle
+  middle,
+  takeUntil,
+  without
 };
